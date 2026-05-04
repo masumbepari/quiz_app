d9a7b5cea447ed46fc0e8c801e70a0c630c14113
masumbepari <masum.fb.2025@gmail.com>
2026-05-04 12:47:19 +0600

    optimize and restucture the code and added two more screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
-
 import 'package:flutter/material.dart';
-import 'package:quiz_app/splash_screen.dart';
+import 'package:quiz_app/quiz.dart';
 
-void main(){
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home:SplashScreen(),
-  ));
+void main() {
+  runApp(Quiz());
 }
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -4,19 +4,7 @@ class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-         gradient: LinearGradient(colors: [
-          const Color.fromARGB(255, 117, 62, 212),
-          const Color.fromARGB(255, 71, 41, 124)
-          ],
-          begin:Alignment.topCenter,
-          end: Alignment.bottomCenter
-          
-          )
-    ),
-    child: Center(
+    return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -31,8 +19,6 @@ class SplashScreen extends StatelessWidget {
            ),
         ],
       ),
-    ),
-    ),
     );
   }
 }
