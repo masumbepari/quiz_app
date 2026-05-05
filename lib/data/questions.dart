258535aa85350eed695e0d5b1e666441dbeeb560
masumbepari <masum.fb.2025@gmail.com>
2026-05-05 17:45:51 +0600

    added 10 flutter question and answer for quiz

---
+++ lib/data/questions.dart
@@ -0,0 +1,83 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion("What is a Widget in Flutter?",
+  [
+    'building block of UI',
+    "function",
+    'variable',
+    'database'
+  ]),
+
+  QuizQuestion("Which widget is used for layout in vertical direction?",
+  [
+    'Column',
+    "Row",
+    'Stack',
+    'Container'
+  ]),
+
+  QuizQuestion("Which widget is used for horizontal layout?",
+  [
+    'Row',
+    "Column",
+    'ListView',
+    'Scaffold'
+  ]),
+
+  QuizQuestion("What does setState() do?",
+  [
+    'Rebuilds the UI',
+    "Deletes widget",
+    'Stops app',
+    'Creates new project'
+  ]),
+
+  QuizQuestion("Which widget provides app structure (AppBar, Body)?",
+  [
+    'Scaffold',
+    "Container",
+    'MaterialApp',
+    'Column'
+  ]),
+
+  QuizQuestion("What is the root widget of a Flutter app?",
+  [
+    'MaterialApp',
+    "Container",
+    'Scaffold',
+    'Text'
+  ]),
+
+  QuizQuestion("Which widget is used to display text?",
+  [
+    'Text',
+    "Container",
+    'Column',
+    'Icon'
+  ]),
+
+  QuizQuestion("Which widget allows scrolling?",
+  [
+    'ListView',
+    "Column",
+    'Row',
+    'Text'
+  ]),
+
+  QuizQuestion("What is used to add padding around a widget?",
+  [
+    'Padding',
+    "Margin",
+    'Align',
+    'Expanded'
+  ]),
+
+  QuizQuestion("Which widget expands to fill available space?",
+  [
+    'Expanded',
+    "Padding",
+    'Center',
+    'Text'
+  ]),
+];
