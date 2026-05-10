e1719324b987e0bb60133c05fcb00a9cbd24711c
masumbepari <masum.fb.2025@gmail.com>
2026-05-10 18:40:53 +0600

    list do not reset after taking first quiz fixed it removeving final variable

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -12,7 +12,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
- final  List<String> selectedAnswers = [];
+  List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
 
   // @override
@@ -31,6 +31,7 @@ class _QuizState extends State<Quiz> {
         selectedAnswers.add(answer);
         if(selectedAnswers.length == questions.length){
           setState(() {
+            selectedAnswers = [];
             activeScreen='start-screen';
           });
         }
